b1b83e15d3503924e629879bb0247606a96a5ede
affan810 <shaikhaffan810@gmail.com>
2025-02-25 16:50:14 +0530

    added abstractclass

--- AccuracyMetric.py
+++ AccuracyMetric.py
@@ -1,6 +1,15 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
 class AccuracyMetric():
+
+    @abstractmethod
+    def AccuracyMetric(Y_test: np.ndarray, predctions: np.ndarray) -> float:
+        '''
+        This is an abstract method that takes two numpy.ndarray Y_test and prediction, compares them by the given accuracy metric and returns a 
+        float represting the respective accuracy. 
+        '''
+        pass
 
     def __init__(self, Y_test, predictions):
         self.Y_test = Y_test
